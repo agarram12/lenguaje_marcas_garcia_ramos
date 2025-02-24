01ed6de4dd75aebf5829b425ca9a961a28a10d33
agarram12 <agarram0711@g.educaand.es>
2025-02-24 20:37:52 +0100

    Añadidos cambios importantes JS

--- Unidad05/DniJS/Script1.js
+++ Unidad05/DniJS/Script1.js
@@ -1,13 +1,14 @@
-function generaLetraDni(numeroDni) {
-    // declarar las letras como constante ya que nunca cambiarán
-    
+function generaLetraDni() {
+    let elementoInput = document.getElementById("dni");
+    let elementoRes = document.getElementById("resultado");
+    let numeroDni = elementoInput.value;
     let letra = 0;
-    let respuesta = "No se trata de un DNI válido";
+    let respuesta = "No se trata de un dni correcto";
     const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
-    if (numeroDni.length == 8 && !isNaN(numeroDni) && Number(numeroDni) > 1 && Number(numeroDni) < 99999999) {
+    if (numeroDni.length == 8 && !isNaN(numeroDni) && Number(numeroDni) >= 1 && Number(numeroDni) <= 99999999) {
         let resto = Number(numeroDni) % 23;
-        let letra = letras[resto];
+        letra = letras[resto];
         respuesta = numeroDni + letra;
     }
-    return respuesta;
+    resultado.innerHTML = respuesta;
 }
