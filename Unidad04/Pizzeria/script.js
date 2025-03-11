e3c8896c3587db432d84727b0f8322442c6f1830
agarram12 <agarram0711@g.educaand.es>
2025-03-11 15:57:11 +0100

    Pizzeria y otros cambios junto a actividad

---
+++ Unidad04/Pizzeria/script.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    console.log();
+    let dir = document.getElementById("direccion").value;
+    let tel = document.getElementById("Telefono").value;
+    let piz = codument.getElementById("pizza").value;
+    if (null!=dir) {
+        console.log(dir);
+    }
+    alert("Stop");
+
+    if(null != piz || piz === "") {
+        
+    }
+}
--- Unidad05/DniJS/Script1.js
+++ Unidad05/DniJS/Script1.js
@@ -1,14 +1,26 @@
 function generaLetraDni() {
+    alert("entra");
     let elementoInput = document.getElementById("dni");
     let elementoRes = document.getElementById("resultado");
     let numeroDni = elementoInput.value;
     let letra = 0;
     let respuesta = "No se trata de un dni correcto";
     const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
+
     if (numeroDni.length == 8 && !isNaN(numeroDni) && Number(numeroDni) >= 1 && Number(numeroDni) <= 99999999) {
         let resto = Number(numeroDni) % 23;
         letra = letras[resto];
         respuesta = numeroDni + letra;
     }
     resultado.innerHTML = respuesta;
+    let elementoBoton = document.getElementsByTagName("button")[0];
+    elementoBoton.classList.remove("activo");
+    elementoBoton.setAttribute("disabled", "true");
+}
+
+function numeroCambiado() {
+    let elementoBoton = document.getElementsByTagName("button")[0];
+    elementoBoton.classList.add("activo");
+    elementoBoton.removeAttribute("disabled")
+    console.log(elementoBoton);
 }
